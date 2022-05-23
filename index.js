909aa42db12a387dca2ca51797385b9ed04b9a08
henrik9999 <22085664+henrik9999@users.noreply.github.com>
2022-05-23 22:46:06 +0200

    update

--- index.js
+++ index.js
@@ -1,49 +1,74 @@
-const fetch = require('node-fetch');
-const fs = require('fs-extra')
-const path = require('path');
-const table = require('markdown-table');
+import fetch from 'node-fetch';
+import fs from 'fs-extra';
+import path from 'path';
+import {markdownTable} from 'markdown-table';
 
 async function start() {
 	fs.removeSync('data');
-	await backup('/page/export', 'pages');
+	const stats = await backup('/page/export', 'pages', 'anime', 1, {}, {});
 	await backup('/mal/export', 'myanimelist');
-	await backup('/ani/export', 'anilist');
-	await updateReadme();
+	await backup('/anilist/export', 'anilist');
+	await updateReadme(stats);
 }
 
 
-async function backup(url, name, type = 'anime', page = 0, ids = {}) {
-	await getData(url + '/' + type + '/' + page).then(async (json) => {
+async function backup(url, name, type = 'anime', page = 1, ids = {}, stats = null) {
+	return await getData(url + '/' + type + '/' + page).then(async (json) => {
 		const promises = json.data.map(data => {
 			if(!ids[data.page ?? type]) {
 				ids[data.page ?? type] = [];
 			}
 			ids[data.page ?? type].push(data.identifier ?? data.id);
+			if(name === 'pages') {
+				if(!stats[data.page]) {
+					stats[data.page] = {
+						total: 0,
+						mal: 0,
+						noMal: 0,
+						ani: 0,
+						noAni: 0
+					}
+				}
+				stats[data.page].total += 1;
+				if(data.malId) {
+					stats[data.page].mal += 1;
+				} else {
+					stats[data.page].noMal += 1;
+				}
+				if(data.aniId) {
+					stats[data.page].ani += 1;
+				} else {
+					stats[data.page].noAni += 1;
+				}
+			}
 			return fs.outputFile(`data/${name}/${data.page ?? type}/${data.identifier ?? data.id}.json`, JSON.stringify(data, null, 2))
 		})
 		await Promise.all(promises);
 
 		if(json.next) {
-			return await backup(url, name, type, page + 1, ids);
+			return await backup(url, name, type, page + 1, ids, stats);
 		} else {
 			if(type === 'anime') {
-				return await backup(url, name, 'manga', 0, ids)
+				return await backup(url, name, 'manga', 1, ids, stats);
 			}
-			for(item in ids) {
-				await fs.outputFile(`data/${name}/${item}/_index.json`, JSON.stringify(ids[item].sort(), null, 2))
+			for(const item in ids) {
+				await fs.outputFile(`data/${name}/${item}/_index.json`, JSON.stringify(ids[item].sort(), null, 2));
 			}
 			if(name === 'pages') {
-				await fs.outputFile(`data/${name}/_index.json`, JSON.stringify(Object.keys(ids).sort(), null, 2))
+				await fs.outputFile(`data/${name}/_index.json`, JSON.stringify(Object.keys(ids).sort(), null, 2));
+				return stats;
 			}
 			return true;
 		}
-	}).catch((e) => {throw 'backup error'});
+	}).catch((e) => {
+		throw 'backup error'
+	});
 }
 
 async function getData(url) {
 	return fetch(process.env.DOMAIN + url, {
 		headers: {
-			'x-access-token': process.env.ACCESS_TOKEN
+			'Authorization': "Permanent " + process.env.ACCESS_TOKEN
 		}
 	}).then(res => {
 		if(res.status !== 200) {
@@ -53,27 +78,25 @@ async function getData(url) {
 	})
 }
 
-async function updateReadme() {
-	getData('/stats/db').then(json => {
-		let stats = [['Page', 'Total', 'MalID', 'noMalID', 'AniID', 'noAniID']];
+async function updateReadme(statsData) {
+	let statsArray = [['Page', 'Total', 'MalID', 'noMalID', 'AniID', 'noAniID']];
 
-		for (const pageName in json.pages) {
-			const page = json.pages[pageName];
-			stats.push([pageName, page.total, page.mal, page.noMal, page.ani, page.noAni])
+	for (const pageName in statsData) {
+		const stats = statsData[pageName];
+		statsArray.push([pageName, stats.total, stats.mal, stats.noMal, stats.ani, stats.noAni])
+	}
+
+	const statstable = markdownTable(statsArray);
+	const descFile = path.resolve('./README.md');
+
+	fs.readFile(descFile, 'utf8', function(err, data) {
+		if (err) {
+			throw err;
 		}
+		const result = data.replace(/<!--statstable-->((.|\n|\r)*)<!--\/statstable-->/g, `<!--statstable-->\n${statstable}\n<!--/statstable-->`);
 
-		const statstable = table(stats);
-		const descFile = path.resolve('./README.md');
-
-		fs.readFile(descFile, 'utf8', function(err, data) {
-			if (err) {
-			  throw err;
-			}
-			const result = data.replace(/<!--statstable-->((.|\n|\r)*)<!--\/statstable-->/g, `<!--statstable-->\n${statstable}\n<!--/statstable-->`);
-
-			fs.writeFile(descFile, result, 'utf8', function(err) {
-			  if (err) throw err;
-			});
+		fs.writeFile(descFile, result, 'utf8', function(err) {
+			if (err) throw err;
 		});
 	});
 }
